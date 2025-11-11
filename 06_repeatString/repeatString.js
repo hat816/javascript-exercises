d1bab024553f53ec20ebd5c114d53c057ad74454
hat816-ual <181215502+hat816@users.noreply.github.com>
2025-11-11 15:32:04 +0100

    Función repeatString completada: - Ahora repite la cadena el número de veces indicado y devuelve 'ERROR' si el número es negativo. - Se mantiene compatibilidad con tests existentes, incluyendo repetición de 0 o 1 veces, y strings vacíos.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (cadena, num) {
+    let resultado = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+       resultado += cadena;
+    }
 
+     return resultado;
 };
 
 // Do not edit below this line
